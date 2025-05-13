91440f37601708b37c873d3e631588cf816e0669
Sermad Mehdi <sermadkharal5@gmail.com>
2025-05-13 22:11:05 +0500

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Welcome to my portfolio website!");
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector(".navbar");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            navbar.style.backgroundColor = "#555";
+        } else {
+            navbar.style.backgroundColor = "#333";
+        }
+    });
+});
